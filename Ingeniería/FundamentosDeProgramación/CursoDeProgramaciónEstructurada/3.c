7a9233bc450c54918c5c51e85661660529631432
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2020-12-26 23:20:46 -0500

    first C code added

---
+++ Ingeniería/FundamentosDeProgramación/CursoDeProgramaciónEstructurada/3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Variables y tipos de datos \n\n");
+
+    int enteroA = 11223;
+    float flotanteA = 1.15;
+    double doubleA = 1.18;
+    char caracterA = 'S';
+
+    printf("El valor de enteroA es: %i \n", enteroA);
+    printf("El valor de flotanteA es: %f \n", flotanteA);
+    printf("El valor de doubleA es: %f \n", doubleA);
+    printf("El valor de caracterA es: %c \n", caracterA);
+
+    return 0;
+}
