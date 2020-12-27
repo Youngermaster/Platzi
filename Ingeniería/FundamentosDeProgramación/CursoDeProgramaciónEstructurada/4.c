a95605a015eb03e76e2f0d715bdb948944d44f9d
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2020-12-26 23:50:26 -0500

    Some little changes

---
+++ Ingeniería/FundamentosDeProgramación/CursoDeProgramaciónEstructurada/4.c
@@ -0,0 +1,19 @@
+// Variables y tipos de datos
+#include <stdio.h>
+
+int main()
+{
+    printf("Variables and data types \n\n");
+
+    int aInteger = 11223;
+    float aFloat = 1.15;
+    double aDouble = 1.18;
+    char aChar = 'S';
+
+    printf("aInteger value is: %i \n", aInteger);
+    printf("aFloat value is: %f \n", aFloat);
+    printf("aDouble value is: %f \n", aDouble);
+    printf("aChar value is: %c \n", aChar);
+
+    return 0;
+}
