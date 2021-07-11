80cd339a660b935ad99c57e38b74085c83f53958
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-07-11 10:35:49 -0500

    Basic type casting in Python

---
+++ Ingeniería/CursoBasicoDePython/15_data_type_casting.py
@@ -0,0 +1,17 @@
+number_1 = input("Type a number: ")
+# ! This is a string
+print(number_1)
+
+# * We can convert it to int like this:
+number_1 = int(input("Type a number: "))
+print(number_1)
+
+number_2 = int(input("Type a number: "))
+print(number_2)
+
+print(number_1 + number_2)
+
+# * Decimal points
+decimal_number = 4.5
+print(int(decimal_number))
+print(str(decimal_number))
