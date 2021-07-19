224a3a06a2836c93027ff99e20cea500754a7f5c
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-07-18 21:34:22 -0500

    Slices added

---
+++ Ingeniería/CursoBasicoDePython/19_slices.py
@@ -0,0 +1,16 @@
+my_name = "Juan Manuel Young Hoyos"
+print(my_name[0])
+
+# * Both of them are equal
+print(my_name[0:3])
+print(my_name[:3])
+
+# * Both of them are equal
+print(my_name[1:len(my_name)])
+print(my_name[1:])
+
+# * Slicing with steps
+print(my_name[1:len(my_name):2])
+
+# * It brings me everything
+print(my_name[::])
