fa962353d2ab7b723121755058f3a4ee36aa75dd
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-12-26 11:20:40 -0500

    Flutter Final Project finished

---
+++ Ingeniería/CursoDeFlutter/platzi_favorite_places/lib/platzi_trips_cupertino.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'home_trips.dart';
+import 'search_trips.dart';
+import 'profile_trips.dart';
+
+class PlatziTripsCupertino extends StatelessWidget {
+  const PlatziTripsCupertino({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: CupertinoTabScaffold(
+        tabBar: CupertinoTabBar(items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home, color: Colors.indigo), title: Text("")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search, color: Colors.indigo), title: Text("")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person, color: Colors.indigo), title: Text("")),
+        ]),
+        tabBuilder: (BuildContext context, int index) {
+          switch (index) {
+            case 0:
+              return CupertinoTabView(
+                builder: (BuildContext context) => HomeTrips(),
+              );
+            case 1:
+              return CupertinoTabView(
+                builder: (BuildContext context) => const SearchTrips(),
+              );
+            case 2:
+              return CupertinoTabView(
+                builder: (BuildContext context) => const ProfileTrips(),
+              );
+            default:
+              return CupertinoTabView(
+                builder: (BuildContext context) => HomeTrips(),
+              );
+          }
+        },
+      ),
+    );
+  }
+}
---
+++ Ingeniería/CursoDeFlutter/platzi_favorite_places/lib/review_list.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'review.dart';
+
+class ReviewList extends StatelessWidget {
+  const ReviewList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Review("assets/images/people.jpg", "Varuna Yasas", "1 review · 5 photos",
+            "There is an amazing place in Sri Lanka"),
+        Review("assets/images/ann.jpg", "Anahí Salgado", "2 review · 5 photos",
+            "There is an amazing place in Sri Lanka"),
+        Review("assets/images/girl.jpg", "Gissele Thomas", "2 review · 2 photos",
+            "There is an amazing place in Sri Lanka"),
+      ],
+    );
+  }
+}
