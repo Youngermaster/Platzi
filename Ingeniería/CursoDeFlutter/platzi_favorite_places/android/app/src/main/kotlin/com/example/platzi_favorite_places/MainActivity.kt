af4bae3e57ed4e6b590a63957e554b56fbcc5b9d
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-12-26 11:44:38 -0500

    Status Bar Color set to 'transparent' on Android

--- Ingeniería/CursoDeFlutter/platzi_favorite_places/android/app/src/main/kotlin/com/example/platzi_favorite_places/MainActivity.kt
+++ Ingeniería/CursoDeFlutter/platzi_favorite_places/android/app/src/main/kotlin/com/example/platzi_favorite_places/MainActivity.kt
@@ -2,5 +2,14 @@ package com.example.platzi_favorite_places
 
 import io.flutter.embedding.android.FlutterActivity
 
+import android.annotation.TargetApi
+import android.os.Build
+import android.os.Bundle
+
+
 class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    this.getWindow().setStatusBarColor(android.graphics.Color.TRANSPARENT);
+  }
 }
